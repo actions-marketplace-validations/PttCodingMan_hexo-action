215bce32f3688492762542b9aefa66a824a78dc6
CodingMan <pttcodingman@gmail.com>
2024-04-18 14:32:53 +0800

    feat: update dockerfile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,15 +19,15 @@ npm install hexo-deployer-git --save
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
-    hexo g -d
+    hexo g --deploy
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
+    hexo g --deploy
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
+    hexo g --deploy -m "$INPUT_COMMIT_MSG"
 fi
 
 echo ::set-output name=notify::"Deploy complate."
